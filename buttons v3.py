bb66dd2511befa8a202921e9c7e5aa2367d09226
Samuel Bennett <bennett.sm89@gmail.com>
2015-10-05 15:50:10 +0100

    Touching up, adding commments for usability

--- buttons v3.py
+++ buttons v3.py
@@ -1,53 +1,53 @@
-
 import numpy as np
+
 import sunpy
 import sunpy.cm
+import astropy.io.fits as pyfits
+
 import pickle
-from datetime import datetime
-import matplotlib.ticker as ticker
+
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button, Cursor
+
 from glob import glob
-from def_spic import spicule
-import astropy.io.fits as pyfits
-5
+from def_prop import properties
+
+
+
 # input the date i'm looking at
-# start at this date sam
+# start at this date
 d = '2013_01_24'
 prompt = '>'
-print "please input the name of the spicule file, remember this is a failsafe in order to not write over recorded data"
+print "please input the name of the pickle file, remember this is a failsafe in order to not write over recorded data"
 sf = raw_input(prompt)
+
 
 
 # set up the initial files and some parameter
 # change these for each data set
-files = glob('/home/sam/Dowloads/*lev1')
-#files = glob('/storage/check/AIA/' + d + '/*.npy' )
-#files = glob('/storage2/users/sam/AIA/' + d + '/*.npy')
+files = glob('/storage2/SDO/jet/*304a*.fits')
 files.sort()
-out = np.load(files[0])[:,:8000]
 
 # read the fits headers
-hdulist = pyfits.open('c:/Users/Samuel/Documents/data/2013_02_01/aia_lev1_304a_2013_02_01t12_55_07_13z_image_lev1_fits')
-#image_lev1_001.fits 
+# used for setting the 
+hdulist = pyfits.open('')
 prihdr = hdulist[0].header
 ondisk = (prihdr['R_SUN'] - 1400)
 limb = ondisk
 
 # the plotting instructions
 fig = plt.figure()
-im = plt.imshow(out,origin='lower',interpolation='nearest',vmax=out[:,ondisk:].max()/18, vmin=out[:,ondisk:].min())
+im = plt.imshow(files[0],origin='lower',interpolation='nearest',vmax=files[0][:,ondisk:].max()/18, vmin=files[0][:,ondisk:].min())
 plt.axhline(limb)
+
+# colour map used matching with SDO/AIA 30.4nm 
 plt.set_cmap(sunpy.cm.get_cmap('sdoaia304'))
-def relabel(x, pos = None):
-    return "%3.2f" % ((x/8000.)*2*np.pi)
 axes = plt.subplot(111)
 plt.subplots_adjust(bottom=0.2)
-formatter = ticker.FuncFormatter(relabel)
-axes.xaxis.set_major_formatter(formatter)
 
-# list all the spicules!
-spic_list = []
+
+# class using the functions 
+prop_list = []
 class Index:
     ind = 0
     plot_points = None
@@ -60,7 +60,7 @@ class Index:
         my_title.set_text(files[i].replace('/',' ').split()[-1][:-4])
         print 'NEXT'
         try:
-            x = np.array(spic_list[-1].points[-1])
+            x = np.array(prop_list[-1].points[-1])
             if self.plot_points:
                self.plot_points.set_data(x[:,0],x[:,1])
             else:
@@ -78,8 +78,8 @@ class Index:
         
     def begin(self, event):
         print 'START SPICULE'
-        spic_list.append(spicule(axes.axis(),limb))
-        print spic_list[-1]
+        prop_list.append(properties(axes.axis(),limb))
+        print prop_list[-1]
         self.spic_no = 0
         return
 
@@ -93,22 +93,23 @@ class Index:
         
     def points(self, event):
         print 'CONFIRM'
-        time = files[self.ind].replace('/',' ').split()[-1][:-4]
-#       real_time = datetime.strptime(time, '%Y_%m_%d_%H_%M_%S')
-#       real_time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%f")
-        print len(spic_list[-1].points)
-        if len(spic_list[-1].points) > 1:
+        tempfile = pyfits.open(files[self.ind])
+        # you will need to change this depending on 
+        # how time is defined in the fits header
+        real_time = tempfile['DATE-OBS']
+
+        print len(prop_list[-1].points)
+        if len(prop_list[-1].points) > 1:
             try:
-                self.abcd[0][1] = spic_list[-1].points[0][0][1]
+                self.abcd[0][1] = prop_list[-1].points[0][0][1]
             except Exception as E:
                 print E
                 import pdb; pdb.set_trace()
                 
-        spic_list[-1].addstep(self.ind, real_time, self.abcd[0], self.abcd[1],
+        prop_list[-1].addstep(self.ind, real_time, self.abcd[0], self.abcd[1],
                               self.abcd[2], self.abcd[3])
-        print spic_list[-1]
+        print prop_list[-1]
     
-    # also this function
     def record(self, event):
         print 'RECORD'
         self.abcd = []
@@ -117,7 +118,6 @@ class Index:
             fig.canvas.mpl_disconnect(self.cid)
         self.cid = fig.canvas.mpl_connect('button_press_event', self.get_click)
         
-    # this looks like the on click
     def get_click(self, event):
         print('you pressed', event.button, event.xdata, event.ydata)
         self.abcd.append([event.xdata, event.ydata])
@@ -125,8 +125,7 @@ class Index:
         self.check_abcd()
     
     def check_abcd(self):
-        if len(self.abcd) == 2:
-            
+        if len(self.abcd) == 2:  
             vec_mid = []
             vec_mid.append((self.abcd[0][0] + self.abcd[1][0])/2.0)
             vec_mid.append((self.abcd[0][1] + self.abcd[1][1])/2.0)
@@ -146,7 +145,7 @@ class Index:
             self.cid = None
             del self.cursor
             print self.abcd
-            axes.axis(spic_list[-1].BBox)
+            axes.axis(prop_list[-1].BBox)
             self.wid_line[-1].set_visible(False)
             plt.draw()
             return True
@@ -154,10 +153,11 @@ class Index:
             return False
             
     def save_im(self, event):
-        time = files[self.ind].replace('/',' ').split()[-1][:-4]
-        print time
+        tempfile = pyfits.open(files[self.ind])
+        real_time = tempfile['DATE-OBS']        
+        print real_time
         # you'll need to change this destiniation to save images of macrospicules
-        # return plt.savefig('/home/sam/Dropbox/Sam/AutoSpic/' + time + '.png')
+        return plt.savefig('/your_file_path_here/AutoSpic/' + real_time + '.png')
         
     def delete(self, event):
         fig.canvas.mpl_disconnect(self.cid)
@@ -186,25 +186,25 @@ class Index:
         return y
 
     def zone1(self, event):
-        spic_list[-1].zone = "Coronal Hole"
+        prop_list[-1].zone = "Coronal Hole"
         
     def zone2(self, event):
-        spic_list[-1].zone = "Coronal Hole Boundary"
+        prop_list[-1].zone = "Coronal Hole Boundary"
 
     def zone3(self, event):
-        spic_list[-1].zone = "Quiet Sun"        
+        prop_list[-1].zone = "Quiet Sun"        
         
     
         
         
-        
 
-time = files[0].replace('/',' ').split()[-1][:-4]
-#real_time = datetime.strptime(time, '%Y_%m_%d_%H_%M_%S')
-#real_time = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%f")
+# set up the figure
+
 plt.xlabel('Theta round the centre of the Sun in Radians')
 plt.ylabel('Arcseconds')
-my_title = plt.title('Spicule on the Date of %s' % time)
+tempfile = pyfits.open(files[0])
+real_time = tempfile['DATE-OBS']
+my_title = plt.title('Spicule on the Date of %s' % real_time)
 
 
 # create some buttons
@@ -264,8 +264,9 @@ plt.show()
 
 
 # save the spicule
-f = open("/storage/check_spics/" + d + "/Spicules/" + sf + ".pik",'wb')
-pickle.dump(spic_list,f)
+# define the file path to save the pickle files out to
+f = open("/home/sam/Dropbox/Sam/test/" + sf + ".pik",'wb')
+pickle.dump(prop_list,f)
 f.close()
 
 print 'Im finished now!'
